718b4c1dd3936d2b473a50a7d475e5966cb5efb9
tangyixiang <851565913@qq.com>
2023-05-19 09:55:30 +0800

    颜色修改

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -1,6 +1,29 @@
 import React, { useEffect, useRef, useId } from 'react'
 import { init, dispose, LineType } from 'klinecharts'
 
+const barStyle = {
+  candle: {
+    // 蜡烛图类型 'candle_solid'|'candle_stroke'|'candle_up_stroke'|'candle_down_stroke'|'ohlc'|'area'
+    type: 'candle_up_stroke',
+    // 蜡烛柱
+    bar: {
+      upColor: '#F46E6B',
+      downColor: '#00A843',
+      noChangeColor: '#888888',
+      upBorderColor: '#F46E6B',
+      downBorderColor: '#00A843',
+      noChangeBorderColor: '#888888',
+      upWickColor: '#F46E6B',
+      downWickColor: '#00A843',
+      noChangeWickColor: '#888888',
+    },
+    xAxis: {
+      show: false,
+    },
+    tooltip: { showRule: 'none' },
+  },
+}
+
 function StockData(props) {
   const chart = useRef()
   const paneId = useRef('')
@@ -21,6 +44,11 @@ function StockData(props) {
       dispose(id)
     }
   }, [])
+
+  useEffect(() => {
+    chart.current?.setStyles(barStyle)
+  }, [])
+
   return <div ref={chart} id={id} className="h-64 w-full" />
   // style={{ width: '620px', height: '440px' }}
 }
