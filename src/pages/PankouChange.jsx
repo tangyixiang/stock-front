5d3b89aa8dd616ea6ab7d0da344050aa4f4fa99b
tangyixiang <851565913@qq.com>
2023-07-04 17:55:28 +0800

    完善异动table

--- src/pages/PankouChange.jsx
+++ src/pages/PankouChange.jsx
@@ -13,6 +13,8 @@ import {
   Row,
   Col,
   Pagination,
+  Modal,
+  Table,
 } from 'antd'
 import axios from 'axios'
 import dayjs from 'dayjs'
@@ -20,19 +22,69 @@ import StockData from '../components/StockData'
 import AFloatButton from '../components/AFloatButton'
 import Wrapper from '../components/Wrapper'
 
+const columns = [
+  {
+    title: '代码',
+    dataIndex: 'symbol',
+    width: 100,
+  },
+  {
+    title: '名称',
+    dataIndex: 'name',
+    width: 150,
+  },
+  {
+    title: '市值',
+    dataIndex: 'marketValue',
+    width: 100,
+    render: (text) => (text / 10000 / 10000).toFixed(2) + '亿',
+  },
+  {
+    title: '次数',
+    dataIndex: 'count',
+    width: 100,
+  },
+  {
+    title: '信息',
+    dataIndex: 'detail',
+    ellipsis: true,
+    render: (detail) => (
+      <span>
+        {detail
+          .map((item) => {
+            const temp = `金额:${item.message.split(',')[1]},数量:${
+              item.message.split(',')[0] / 100
+            }手`
+            return temp
+          })
+          .join('\n')}
+      </span>
+    ),
+  },
+]
+
 const PankouChange = () => {
   const today = dayjs().format('YYYY-MM-DD')
   const [data, setData] = useState([])
   const [pageInfo, setPageInfo] = useState({ pageSize: 20, pageNo: 1 })
   const [total, setTotal] = useState(0)
-
+  const [modal, setModal] = useState(false)
   const [date, setDate] = useState(today)
+  const [rankData, setRankData] = useState()
   const [info, setInfo] = useState('')
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     getData()
   }, [pageInfo])
+
+  useEffect(() => {
+    axios
+      .get('/api/cn/analysis/pankou/change/counts', {
+        params: { date: date },
+      })
+      .then((res) => setRankData(res.data))
+  }, [date])
 
   const getData = () => {
     setLoading(true)
@@ -89,6 +141,9 @@ const PankouChange = () => {
           >
             查询
           </Button>
+          <Button type="primary" onClick={() => setModal(true)}>
+            显示排行
+          </Button>
         </Space>
         <Divider />
         <Row gutter={[16, 16]}>
@@ -107,7 +162,7 @@ const PankouChange = () => {
                 }}
                 extra={
                   <Space>
-                    <Typography.Text>次数:{item.detail.length}</Typography.Text>
+                    <Typography.Text>次数:{item.count}</Typography.Text>
                     <Popover content={info} trigger="click">
                       <Button onClick={() => getInfo(item.detail)}>详情</Button>
                     </Popover>
@@ -137,6 +192,14 @@ const PankouChange = () => {
         )}
       </Space>
       <AFloatButton watch={date} />
+      <Modal
+        open={modal}
+        onCancel={() => setModal(false)}
+        footer={null}
+        width={'60%'}
+      >
+        <Table columns={columns} dataSource={rankData} rowKey={'symbol'} />
+      </Modal>
     </Wrapper>
   )
 }
